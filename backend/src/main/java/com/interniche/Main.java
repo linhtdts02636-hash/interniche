834531fe46e79bf7ffcf9ee411008afa94b59cf0
Duy Linh <linhtdts02636@gmail.com>
2026-08-22 04:08:34 +0700

    init

--- backend/src/main/java/com/interniche/Main.java
+++ backend/src/main/java/com/interniche/Main.java
@@ -1,3 +1,5 @@
+package com.interniche;
+
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world");
--- backend/src/test/java/com/interniche/InternicheApplicationTests.java
+++ backend/src/test/java/com/interniche/InternicheApplicationTests.java
@@ -1,10 +1,10 @@
-package com.cafphy.backend;
+package com.interniche;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class InternicheApplicationTests {
 
 	@Test
 	void contextLoads() {
