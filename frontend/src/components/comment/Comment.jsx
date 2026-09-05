8a9177eccf24756160dd4e797bc8f10f9018b938
HKBienDev <bienhkts02569@gmail.com>
2026-09-05 21:00:22 +0700

    Keep branch

---
+++ frontend/src/components/comment/Comment.jsx
@@ -0,0 +1,23 @@
+// function Comment ({ id, avatar, username, content, time, likes, isLiked, dislike, isDisliked, replies, onLike, onDislike, onReply }) {
+
+//     return (
+//         <div className="comment">
+//             <div className="comment-header">
+//                 );
+// }
+
+// export default Comment;
+
+function Comment ({id, avatar, username, content, time, likes, isLiked, dislikes, isDisliked, replies, onLike, onDislike, onReply }) {
+    const ;
+    return (
+        <div className="Comment">
+            <div className="comment-header">
+                
+                </div>
+        </div>
+
+    );
+}
+
+export default Comment;
